e3ae37145f3a74f19413c253f91eb224c382050d
enya_enya <hellosilicium@gmail.com>
2018-12-05 22:35:42 +0700

    add game calculate + refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-const readlineSync = require('readline-sync');
+import readlineSync from 'readline-sync';
 
 const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
+
+const getRandomOperation = () => {
+  switch (getRandomNumber(1, 3)) {
+    case 1:
+      return '+';
+    case 2:
+      return '-';
+    default: return '*';
+  }
+};
+
+const calculate = (operator, num1, num2) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    default: return num1 * num2;
+  }
+};
+
+const isEven = number => number % 2 === 0;
 
 export const greeting = (str) => {
   console.log('Welcome to the Brain Games!');
@@ -12,24 +34,55 @@ export const greeting = (str) => {
   return userName;
 };
 
-export const brainEven = () => {
-  const userName = greeting('Answer "yes" if number even otherwise answer "no".\n');
-  let currentAttempt = 0;
+export const engine = (greetingStr, game) => {
+  const userName = greeting(greetingStr);
   const numOfAttempts = 3;
-  const min = 1; // minimum of range
-  const max = 100; // maximum of range
-  while (currentAttempt < numOfAttempts) {
-    const num = getRandomNumber(min, max);
-    const isEven = (num % 2 === 0);
-    const enterNum = readlineSync.question(`${num}\n`);
-    const correctAnswer = (isEven === true ? 'yes' : 'no');
-    if (((isEven === true) && (enterNum === 'yes')) || ((isEven === false) && (enterNum === 'no'))) {
-      currentAttempt += 1;
+  let success = true;
+  for (let i = 0; i < numOfAttempts; i += 1) {
+    success = game();
+    if (success) {
       console.log('Correct!');
     } else {
-      console.log(`'${enterNum}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n Let's try again, ${userName}!`);
-      return;
+      console.log(`Let's try again, ${userName}!`);
+      break;
     }
   }
-  console.log(`Congratulations, ${userName}!`);
+  if (success) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+const checkFunction = (enterAnswer, correctAnswer) => {
+  const success = enterAnswer === correctAnswer;
+  if (!success) {
+    console.log(`'${enterAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  }
+  return success;
+};
+
+const askQuestion = question => readlineSync.question(`Question: ${question}\nYour answer: `);
+
+
+export const brainEven = () => {
+  engine('Answer "yes" if number even otherwise answer "no".\n',
+    () => {
+      const question = getRandomNumber(1, 100);
+      // const enterAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+      const enterAnswer = askQuestion(question);
+      const correctAnswer = (isEven(question) ? 'yes' : 'no');
+      return checkFunction(enterAnswer, correctAnswer);
+    });
+};
+
+export const brainCalc = () => {
+  engine('What is the result of the expression?.\n',
+    () => {
+      const num1 = getRandomNumber(1, 100);
+      const num2 = getRandomNumber(1, 100);
+      const operator = getRandomOperation();
+      const correctAnswer = calculate(operator, num1, num2);
+      const enterAnswer = askQuestion(`${num1} ${operator} ${num2}`);
+      // const enterAnswer = readlineSync.question(`Question: ${num1} ${operator} ${num2} \nYour answer: `);
+      return checkFunction(parseInt(enterAnswer, 10), correctAnswer);
+    });
 };
